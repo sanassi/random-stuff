3fbe79be867ae6d26cad35a6b3ce5581ea7a2b0d
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2023-07-26 14:16:36 +0200

    feat: added differenet icons for project links
    Signed-off-by: Sanassi Cissé <sanassi-mory.cisse@epita.fr>

---
+++ src/Experiences.js
@@ -0,0 +1,27 @@
+import Experience from "./Experience";
+
+export default function Experiences() {
+  return (
+    <div>
+      <label className='experience-label'>Experience</label>
+      <div id='experiences' className="experiences">
+        <Experience duration={{'start' : 'Jan. 2024', 'end' : 'July 2024'}}
+                    title='Teaching Assistant (YAKA)'
+                    description={['']}
+                    company='Epita'
+                    techs={['Java']}/>
+        <Experience duration={{'start' : 'Sept. 2023', 'end' : 'Jan. 2024'}}
+                    title='Backend Developer Intern'
+                    description={['']}
+                    company='Rakuten France'
+                    techs={['Java']}/>
+        <Experience duration={{'start' : 'Sept. 2022', 'end' : 'July 2023'}}
+                    title='Teaching Assistant (ACDC)'
+                    description={['Teaching computer science and programming to 1st year students.',
+                      'Writing, testing and deploying weekly programming practicals for students in C#']}
+                    company='Epita Paris'
+                    techs={['C#']}/>
+      </div>
+    </div>
+  );
+};
---
+++ src/Projects.js
@@ -0,0 +1,44 @@
+import ProjectInfo from "./ProjectInfo";
+
+import './Projects.css'
+export default function Projects() {
+  return (
+    <div>
+      <label className='project-label'>Projects</label>
+      <div id='projects' className="projects">
+        <ProjectInfo name="Tiger Compiler Backend"
+                     lang={["C++"]}
+                     description={['Compiler for the Tiger Language.', '14 weeks project.']}
+                     link={{
+                       "site": "other",
+                       "url": "https://assignments.lrde.epita.fr/tools/modern_compiler_implementation/first_editions.html"
+                     }} />
+        <ProjectInfo name="Rash"
+                     lang={["C"]}
+                     description={['Shell Command Line Interpreter.']}
+                     link={{
+                       "site":"gitlab",
+                       "url":"https://gitlab.com/sanassi/rash"}} />
+        <ProjectInfo name="Red Panda IDE"
+                     lang={["Java", "JavaFX"]}
+                     description={['IDE for beginners',
+                       'Autocomplete', 'Syntax Highlighting']}
+                     link={{
+                       "site":"github",
+                       "url":"https://github.com/sanassi/red-panda"}} />
+        <ProjectInfo name="PixElArt"
+                     lang={["Javascript"]}
+                     description={['Pixel Art Editor', 'Built with Vanilla Javascript']}
+                     link={{
+                       "site":"github",
+                       "url":"https://github.com/sanassi/pixElArt"}} />
+        <ProjectInfo name="Portfolio Website"
+                     lang={["ReactJS"]}
+                     description={["First iteration of my portfolio website"]}
+                     link={{
+                       "site":"github",
+                       "url":"https://github.com/sanassi/random-stuff"}} />
+      </div>
+    </div>
+  );
+};
